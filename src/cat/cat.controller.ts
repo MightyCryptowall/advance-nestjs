4dede81aeedddb472ee412fcc76bc122d6468660
Ye Thiha <mightycryptowall@gmail.com>
2021-09-01 12:53:46 +0630

    cat service & interface -a

--- src/cat/cat.controller.ts
+++ src/cat/cat.controller.ts
@@ -1,8 +1,11 @@
+import { CatService } from './cat.service';
 import { Controller, Delete, Get, Post } from '@nestjs/common';
 
 @Controller('cat')
 export class CatController {
-    
+    constructor(
+        private readonly catService:CatService
+    ){}
     @Post()
     async createCat() {
         return [];
--- src/cat/cat.service.ts
+++ src/cat/cat.service.ts
@@ -1,4 +1,16 @@
+import { Cat } from './cat.interface';
+
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CatService {}
+export class CatService {
+    private readonly cats:Cat[] = [];
+
+    create(cat: Cat){
+        this.cats.push(cat);
+    }
+
+    findAll(): Cat[] {
+        return this.cats;
+    }
+}
